a42e3a6423502a2f58dcc4a61c636b420216b077
Brad Whitted <whittedb@the-zoo.net>
2019-09-14 15:20:33 -0500

    First rev of FastLED implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,48 @@
 #include <Arduino.h>
 #include <util/atomic.h>
-#include <Adafruit_NeoPixel.h>
+//#include <Adafruit_NeoPixel.h>
+#include <FastLED.h>
 
 
 constexpr auto LED_CNT = 24;
 constexpr auto DRIVER_PIN = 4;
 constexpr auto ON_OFF_PIN = 2;
 
-enum State {
+enum State
+{
   S_OFF,
   S_ON,
   S_TURN_ON,
-  S_TURN_OFF
+  S_TURN_OFF,
+/*
+  S_FADE_ON,
+  S_WAIT_FADE_ON,
+  S_FADE_OFF,
+  S_WAIT_FADE_OFF,
+*/
 };
 
 volatile State state = S_OFF;
-Adafruit_NeoPixel eyes(LED_CNT, DRIVER_PIN, NEO_GRBW);
+//Adafruit_NeoPixel eyes(LED_CNT, DRIVER_PIN, NEO_GRBW);
+CRGB leds[LED_CNT];
 
 void debounceButton();
 
 void setup()
 {
-  eyes.begin();            // INITIALIZE NeoPixel strip object (REQUIRED)
-  eyes.show();             // Turn OFF all pixels ASAP
-  eyes.setBrightness(128); // Set BRIGHTNESS (max = 255)
+  delay(2000);  // Wait for NeoPixel Ring to initialize
 
+  FastLED.addLeds<NEOPIXEL, DRIVER_PIN>(leds, LED_CNT);
+  FastLED.clear(true);
+  FastLED.setBrightness(0);
+  for (int i = 0; i < LED_CNT; ++i) {
+    leds[i].setRGB(255, 0, 0);
+  }
+  /*
+    eyes.begin();            // INITIALIZE NeoPixel strip object (REQUIRED)
+    eyes.show();             // Turn OFF all pixels ASAP
+    eyes.setBrightness(128); // Set BRIGHTNESS (max = 255)
+  */
   pinMode(ON_OFF_PIN, INPUT_PULLUP);
 
   attachInterrupt(digitalPinToInterrupt(ON_OFF_PIN), debounceButton, FALLING);
@@ -38,15 +56,63 @@ void loop() {
       break;
 
     case S_TURN_ON:
-      eyes.fill(eyes.Color(255, 0, 0, 0));
+//      eyes.fill(eyes.Color(255, 0, 0, 0));
+      FastLED.show(255);
       ATOMIC_BLOCK(ATOMIC_RESTORESTATE) { state = S_ON; }
       break;
 
     case S_TURN_OFF:
-      eyes.fill(eyes.Color(0, 0, 0, 0));
+//      eyes.fill(eyes.Color(0, 0, 0, 0));
+      FastLED.show(0);
       ATOMIC_BLOCK(ATOMIC_RESTORESTATE) { state = S_OFF; }
       break;
+/*
+    case S_FADE_ON:
+      currentEyePWM =
+        currentEyePWM > MAX_EYE_LEVEL ? MAX_EYE_LEVEL : currentEyePWM;
+      analogWrite(pin, currentEyePWM);
+      state = S_WAIT_FADE_ON;
+      timer.start(FADE_DELAY);
+      break;
 
+    case S_WAIT_FADE_ON:
+      if (timer.expired()) {
+        if (currentEyePWM < MAX_EYE_LEVEL) {
+          currentEyePWM += FADE_STEP;
+          state = S_FADE_ON;
+        } else {
+          state = S_IDLE;
+        }
+      }
+      break;
+
+    case S_FADE_OFF:
+      currentEyePWM =
+        currentEyePWM < MIN_EYE_LEVEL ? MIN_EYE_LEVEL : currentEyePWM;
+      analogWrite(pin, currentEyePWM);
+      state = S_WAIT_FADE_OFF;
+      timer.start(FADE_DELAY);
+      break;
+
+    case S_WAIT_FADE_OFF:
+      if (timer.expired()) {
+        if (currentEyePWM > MIN_EYE_LEVEL) {
+          currentEyePWM -= FADE_STEP;
+          state = S_FADE_OFF;
+        } else {
+          DEBUG_PRINTLN("Eye dim done....");
+          analogWrite(pin, 0);
+          if (systemShuttingDown) {
+            systemShuttingDown = false;
+            firstTime = true;
+            state = S_OFF;
+          } else {
+            state = S_IDLE;
+          }
+        }
+      }
+      break;
+*/
     default:
       break;
   }
